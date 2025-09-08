40d0bed719724bdfa9b704f1aa0891183fdb7044
alfonsomartinezdev <alfonso@alfonsoalfonso.dev>
2025-09-07 17:39:57 -0700

    lint

--- db/migrate/20250908002921_add_remindable_to_reminders.rb
+++ db/migrate/20250908002921_add_remindable_to_reminders.rb
@@ -1,5 +1,5 @@
 class AddRemindableToReminders < ActiveRecord::Migration[8.0]
   def change
-    add_index :reminders, [:remindable_type, :remindable_id]
+    add_index :reminders, [ :remindable_type, :remindable_id ]
   end
 end
